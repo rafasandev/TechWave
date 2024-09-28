4672a926a02f3af1e34ebbad3779f2bb4f19547f
rafaki01 <rs7951006@gmail.com>
2024-09-28 11:48:12 -0300

    Inicio recuperação dados terminal

--- interface/pyqt/terminal.py
+++ interface/pyqt/terminal.py
@@ -3,29 +3,34 @@ from PyQt6.QtGui import QKeyEvent
 from PyQt6.QtWidgets import QWidget, QTextEdit, QVBoxLayout
 from PyQt6.QtCore import Qt
 
+
 class Terminal(QWidget):
-    
+
     def __init__(self):
         super().__init__()
 
         self.terminal = QTextEdit()
-        self.terminal.setStyleSheet("""
+        self.terminal.setStyleSheet(
+            """
             background-color: #272E25; 
             padding: 5px; 
             color: #ffffff;
             border: none;
-        """)
+            font-weight: bold;
+            font-size: 14px;
+        """
+        )
 
+        self.terminal.textChanged.connect(self.getTerminalContent)
 
         self.terminal_layout = QVBoxLayout()
         self.terminal_layout.addWidget(self.terminal)
-        
+
         self.terminal_layout.setSpacing(0)
-        self.terminal_layout.setContentsMargins(0,0,0,0)
+        self.terminal_layout.setContentsMargins(0, 0, 0, 0)
 
         self.setLayout(self.terminal_layout)
 
-    def keyPressEvent(self, keyEvent: QKeyEvent):
-        print(keyEvent.key())
-        # if keyEvent.key() == Qt.Key_Return:
-            # print("Enter")
+    def getTerminalContent(self):
+        print(self.terminal.toHtml())
+        print(self.terminal.toHtml().index("<p"))
