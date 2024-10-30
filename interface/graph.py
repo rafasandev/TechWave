73aaecc71eff74476f99653dfb786470162bf0e7
rafaki01 <rs7951006@gmail.com>
2024-10-30 18:00:34 -0300

    alterações logica criação de grafos

--- interface/graph.py
+++ interface/graph.py
@@ -8,113 +8,47 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from models.vertice import *
 
 categorias = {
-    "Frontend": "#FF9F80 ",
-    "Backend": "#80FF9F ",
-    "Database": "#9F80FF ",
-    "Security": "#FFE680 ",
-    "DevOps": "#80FFFF ",
+    "Categoria 1": "#FFB3B3",
+    "Categoria 2": "#FF6666",
+    "Categoria 3": "#FF3333",
+    "Categoria 4": "#B3FFB3",
+    "Categoria 5": "#66FF66",
+    "Categoria 6": "#33FF33",
+    "Categoria 7": "#B3B3FF",
+    "Categoria 8": "#6666FF",
+    "Categoria 9": "#3333FF",
+    "Categoria 10": "#FFE0B3",
+    "Categoria 11": "#FF9966",
+    "Categoria 12": "#FF6600",
 }
 
 
-# Função para gerar valores aleatórios
 def gerar_valor():
-    return random.randint(0, 200)  # Valores entre 5 e 100 para as arestas
+    return random.randint(0, 200)
 
 
-# Função para escolher uma categoria aleatória
 def escolher_categoria():
     return random.choice(list(categorias.keys()))
 
 
 def escolher_tecnologia():
-    tecnologias = [
-        "Dependencia 1",
-        "Dependencia 2",
-        "Dependencia 3",
-        "Dependencia 4",
-        "Dependencia 5",
-        "Dependencia 6",
-        "Dependencia 7",
-        "Dependencia 8",
-        "Dependencia 9",
-        "Dependencia 10",
-        "Dependencia 11",
-        "Dependencia 12",
-        "Dependencia 13",
-        "Dependencia 14",
-        "Dependencia 15",
-        "Dependencia 16",
-        "Dependencia 17",
-        "Dependencia 18",
-        "Dependencia 19",
-        "Dependencia 20",
-        "Dependencia 21",
-        "Dependencia 22",
-        "Dependencia 23",
-        "Dependencia 24",
-        "Dependencia 25",
-        "Dependencia 26",
-        "Dependencia 27",
-        "Dependencia 28",
-        "Dependencia 29",
-        "Dependencia 30",
-        "Dependencia 31",
-        "Dependencia 32",
-        "Dependencia 33",
-        "Dependencia 34",
-        "Dependencia 35",
-    ]
+    tecnologias = []
+    for tec in range(50):
+        tecnologias.append("Dependência " + str(tec))
     return random.choice(tecnologias)
 
 
-vertices_data = [
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-    (escolher_tecnologia(), escolher_tecnologia(), gerar_valor(), escolher_categoria()),
-]
+vertices_data = []
+
+for num in range(50):
+    vertices_data.append(
+        (
+            escolher_tecnologia(),
+            escolher_tecnologia(),
+            gerar_valor(),
+            escolher_categoria(),
+        )
+    )
 
 
 def create_graph_complete(raw_data):
@@ -128,13 +62,13 @@ def create_graph_complete(raw_data):
             category=vertice.category,
             color=definir_cor_vertice(vertice.category),
             size=20,
-            title="Categoria: " + vertice.category,
+            title=vertice.generate_html_connections(),
             group=vertice.category,
         )
 
         Graph.add_edge(
             vertice.name,
-            vertice.connection,
+            vertice.connections,
             weight=(vertice.value / 10) + 5,
             color="rgba(208,204,202,0.8)",
         )
--- models/Vertice.py
+++ models/Vertice.py
@@ -1,9 +1,14 @@
 class Vertice:
-    def __init__(self, name, value, category, connection):
+    def __init__(self, name, value, category, connections):
         self.name = name
         self.value = value
         self.category = category
-        self.connection = connection
+        self.connections = connections
+
+    def generate_html_connections(self):
+        html_string = ""
+        for conn in self.connections:
+            html_string = html_string + " <br> " + conn
 
     def __str__(self):
         return f"{{'name': '{self.name}','value': '{self.value}''category': '{self.category}''connection': '{self.connection}'}}"
